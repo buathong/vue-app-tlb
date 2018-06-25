134a3fb1b71448bcd59e3accea34726cb55ad665
buathong <buathong>
2018-06-25 10:24:12 +0700

    updeat-table

--- pages/table.vue
+++ pages/table.vue
@@ -1,17 +1,40 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    hide-actions
-    class="elevation-1"
-  >
-    <template slot="items" slot-scope="props">
-      <td>{{ props.item.name }}</td>
-      <td class="text-xs-right">{{ props.item.calories }}</td>
-      <td class="text-xs-right">{{ props.item.fat }}</td>
-      <td class="text-xs-right">{{ props.item.carbs }}</td>
-      <td class="text-xs-right">{{ props.item.protein }}</td>
-      <td class="text-xs-right">{{ props.item.iron }}</td>
-    </template>
-  </v-data-table>
+  <div><h1>Page Login</h1>
+    <v-form v-model="valid">
+      <v-text-field
+        v-model="name"
+        :rules="nameRules"
+        :counter="10"
+        label="Name"
+        required
+      />
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      />
+    </v-form>
+    <v-btn color="success">Save</v-btn>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      id: 1,
+      valid: false,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 10 || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid',
+      ],
+    }
+  },
+}
+</script>
