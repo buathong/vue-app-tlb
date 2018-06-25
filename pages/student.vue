30a1062acb2f178c1a4783abb0b15139179ba766
buathong <buathong>
2018-06-25 09:44:19 +0700

    updeat-link1

--- pages/student.vue
+++ pages/student.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <nuxt-link to="/tabled">Buathong->click.</nuxt-link><br>
-    <router-link :to="{ path: '/student1'}" replace>Register</router-link><br>
-    <router-link :to="{ path: '/student1'}" push>Register</router-link><br>
+    <nuxt-link to="/tabled"><center>Buathong->click.</center></nuxt-link><br>
+    <router-link :to="{ path: '/student1'}" replace><center>Register</center></router-link><br>
+    <router-link :to="{ path: '/student1'}" push><center>Register</center></router-link><br>
   </div>
 </template>
 <script>
