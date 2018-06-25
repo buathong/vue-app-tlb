454f7627c59a7940241fc4a3dcb5a8247831e0ee
buathong <buathong>
2018-06-25 10:15:40 +0700

    updeat-table1

---
+++ pages/Ttest.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    class="elevation-1"
+  >
+    <template slot="headerCell" slot-scope="props">
+      <v-tooltip bottom>
+        <span slot="activator">
+          {{ props.header.text }}
+        </span>
+        <span>
+          {{ props.header.text }}
+        </span>
+      </v-tooltip>
+    </template>
